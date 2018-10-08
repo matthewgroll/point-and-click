111b66d0ec99a08dcd7e6d43a9137885aa5ecb98
Matthew Groll <43278304+matthewgroll@users.noreply.github.com>
2018-10-07 22:03:00 -0400

    Update point_and_click.py

--- point_and_click.py
+++ point_and_click.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 pygame.init()
 # 800 by 600 default
@@ -9,6 +10,8 @@ cowboy_width = 60
 cowboy_height = 83
 cactus_width = 86
 cactus_height = 94
+needle_width = 96
+needle_height = 42
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -18,32 +21,27 @@ gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Point and Click')
 clock = pygame.time.Clock()
 
-
-def cowboy(x_val_cow, y_val_cow):
-    gameDisplay.blit(cowboyImg, (x_val_cow, y_val_cow))
-
-
-# state: 0 = normal, 1 = damaged
-cactus_state = 0
-
-
-def cactus(x_val_cact, y_val_cact):
-    if cactus_state == 0:
-        gameDisplay.blit(cactusImg_1, (x_val_cact, y_val_cact))
-    if cactus_state == 1:
-        gameDisplay.blit(cactusImg_2, (x_val_cact, y_val_cact))
+cowboyImg = pygame.image.load('images/cowboy.png')
+cactusImg_1 = pygame.image.load('images/cactus_1.png')
+cactusImg_2 = pygame.image.load('images/cactus_2.png')
+pointerImg = pygame.image.load('images/crosshair.png')
+fireImg = pygame.image.load('images/crosshair_fired.png')
+hpImg = pygame.image.load('images/hp_bar.png')
+needleImg = pygame.image.load('images/needle.png')
 
 
-def cursor(x_val_mouse, y_val_mouse):
-    gameDisplay.blit(pointerImg, (x_val_mouse - 96/2, y_val_mouse - 96/2))
+def display(img):
+    def display_func(x, y):
+        gameDisplay.blit(img, (x, y))
+    return display_func
 
 
-def cursor_fired(x_val_mouse, y_val_mouse):
-    gameDisplay.blit(fireImg, (x_val_mouse - 96/2, y_val_mouse - 96/2))
-
-
-def hp_bar(x_val_bar, y_val_bar):
-    gameDisplay.blit(hpImg, (x_val_bar, y_val_bar))
+cowboy = display(cowboyImg)
+cactus = display(cactusImg_1)
+cursor = display(pointerImg)
+cursor_fired = display(fireImg)
+hp_bar = display(hpImg)
+needle = display(needleImg)
 
 
 def message_display(text, x_pos, y_pos):
@@ -55,14 +53,6 @@ def message_display(text, x_pos, y_pos):
     talk(x_pos, y_pos)
 
 
-cowboyImg = pygame.image.load('images/cowboy.png')
-cactusImg_1 = pygame.image.load('images/cactus_1.png')
-cactusImg_2 = pygame.image.load('images/cactus_2.png')
-pointerImg = pygame.image.load('images/crosshair.png')
-fireImg = pygame.image.load('images/crosshair_fired.png')
-hpImg = pygame.image.load('images/hp_bar.png')
-needleImg = pygame.image.load('images/needle.png')
-
 def game_loop():
     starting_x = display_width * 0.15
     starting_y = display_height * 0.37
@@ -70,6 +60,8 @@ def game_loop():
     y = starting_y
     cact_x = (display_width * 0.75)
     cact_y = (display_height * 0.35)
+    needle_x = cact_x
+    needle_y = cact_y + random.randint(-20, 20)
     bar_x = 5
     bar_y = display_height - 100
 
@@ -80,8 +72,10 @@ def game_loop():
 
     x_change = 0
     y_change = 0
+    needle_x_change = 0
 
     while not game_exit:
+        gameDisplay.fill(black)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -99,6 +93,8 @@ def game_loop():
                     y_change += speed
                 if event.key == pygame.K_z:
                     talking = True
+                if event.key == pygame.K_f:
+                    needle_x_change = 5
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
@@ -112,9 +108,11 @@ def game_loop():
 
         x += x_change
         y += y_change
+        needle_x -= needle_x_change
+
         if hp <= 0:
             game_over = True
-        gameDisplay.fill(black)
+
         # draw basic shapes for background
         pygame.draw.line(gameDisplay, white, (0, display_height * 0.65), (display_width, display_height * 0.65))
         # player box confines
@@ -141,12 +139,24 @@ def game_loop():
             hp_bar(bar_x + 30*num, bar_y)
         if talking:
             message_display("Hello!", cact_x - 20, cact_y - 20)
+        center_bal = 96/2
+        # manage cursor display
         if pygame.mouse.get_pressed()[0]:
-            cursor_fired(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+            cursor_fired(pygame.mouse.get_pos()[0] - center_bal, pygame.mouse.get_pos()[1] - center_bal)
         else:
-            cursor(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+            cursor(pygame.mouse.get_pos()[0] - center_bal, pygame.mouse.get_pos()[1] - center_bal)
+        # manage collision with needles
+
+        player_hitbox = pygame.Rect(x, y, cowboy_width - 15, cowboy_height)
+        pygame.draw.rect(gameDisplay, white, player_hitbox, 2)
+        needle_hitbox = pygame.Rect(needle_x, needle_y, needle_width, needle_height)
+        pygame.draw.rect(gameDisplay, white, needle_hitbox, 2)
+        needle(needle_x, needle_y)
+        if player_hitbox.colliderect(needle_hitbox):
+            hp -= 1
         pygame.display.update()
-        clock.tick(60)
+        # fps
+        clock.tick(30)
 
 
 game_loop()
